1b92f0f807fd7bcb78e32bdc6240baab822f8ad9
fabbbbz <89973033+fabbbbz@users.noreply.github.com>
2021-10-27 17:37:04 +0200

    Update Orders.js

--- models/Orders.js
+++ models/Orders.js
@@ -8,12 +8,5 @@ var OrdersSchema = mongoose.Schema({
 	status: String,
 })
 
-<<<<<<< HEAD
 var Order = mongoose.model('Orders', OrdersSchema);
-
 module.exports = Order;
-=======
-var Order = mongoose.model('Orders', OrdersSchema)
-
-module.exports = Order
->>>>>>> 6752d2c7df04010ab846deef03bda75ad2050fd8
