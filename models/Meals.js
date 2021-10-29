86b26cf9ae8e0e800dd61bd18ebb3a6ec07b4890
Marion Saul <marionsaul.dev@gmail.com>
2021-10-29 10:30:38 +0200

    upgrade models to only accept specific values as diet and mood

--- models/Meals.js
+++ models/Meals.js
@@ -5,8 +5,26 @@ var mealsSchema = mongoose.Schema({
 	restaurants: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
 	price: Number,
 	ingredients: [{ type: String }],
-	regimeAlim: [{ type: String }],
-	mood: [String],
+	regimeAlim: {
+		type: [String],
+		enum: {
+			values: ['omni', 'vegetarian', 'vegan', 'halal', 'cacher'],
+			message: 'Not a valid diet',
+		},
+	},
+	mood: {
+		type: [String],
+		enum: {
+			values: [
+				'soir de match',
+				'comme chez maman',
+				'healthy',
+				'a partager',
+				'cuisine du monde',
+			],
+			message: 'Not a valid mood',
+		},
+	},
 })
 
 var Meal = mongoose.model('Meals', mealsSchema)
