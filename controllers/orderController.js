7372ca6f3dbfd392f36cfefab090c52f399f41e4
sebastien K <sebastienk21@gmail.com>
2021-11-03 17:50:09 +0100

    stripe allmost ok

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,6 +3,12 @@ const User = require('../models/Users')
 const Order = require('../models/Orders')
 const Meal = require('../models/Meals')
 const Restaurant = require('../models/Restaurants')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+const stripe = require('stripe')
+const stripePK = process.env.PUBLISHABLE_KEY
+const stripeSK = (process.env.SECRET_KEY, { apiVersion: "2020-08-27" })
 
 // Distance max de livraison, en km
 const MAX_DISTANCE = 3
@@ -33,12 +39,12 @@ exports.makeOrder = async (req, res, next) => {
 			req.body.mood !== 'all'
 				? req.body.mood
 				: [
-						'healthy',
-						'soir de match',
-						'comme chez maman',
-						'cuisine du monde',
-						'a partager',
-				  ]
+					'healthy',
+					'soir de match',
+					'comme chez maman',
+					'cuisine du monde',
+					'a partager',
+				]
 
 		const lng = req.body.coords.lat
 		const lat = req.body.coords.lng
@@ -195,5 +201,59 @@ exports.makeOrderInFav = async (req, res, next) => {
 	} catch (err) {
 		res.statusCode = 400
 		res.json({ result: 'fail', err: err.message })
+	}
+}
+exports.payment = async (req, res, next) => {
+	const stripe = require('stripe')('sk_test_51JrTrKGYLeZVv03JtV4ehIwMYnh4ZbIWpgUDdiIZlMi0OLOeGhlfcCeznynmhLCYv1vVizIvaLK5d8TI8hoa1MoM00vwNDE24q');
+	const { paymentMethodType, currency } = req.body;
+
+	// Each payment method type has support for different currencies. In order to
+	// support many payment method types and several currencies, this server
+	// endpoint accepts both the payment method type and the currency as
+	// parameters.
+	//
+	// Some example payment method types include `card`, `ideal`, and `alipay`.
+	const params = {
+		payment_method_types: [paymentMethodType],
+		amount: req.body.price,
+		currency: "eur",
+	}
+	const paymentIntent = await stripe.paymentIntents.create({
+		payment_method_types: ['card'],
+		amount: params.amount,
+		currency: params.currency,
+	});
+	const clientSecret = paymentIntent.client_secret
+	// If this is for an ACSS payment, we add payment_method_options to create
+	// the Mandate.
+	if (paymentMethodType === 'acss_debit') {
+		params.payment_method_options = {
+			acss_debit: {
+				mandate_options: {
+					payment_schedule: 'sporadic',
+					transaction_type: 'personal',
+				},
+			},
+		}
+	}
+
+	// Create a PaymentIntent with the amount, currency, and a payment method type.
+	//
+	// See the documentation [0] for the full list of supported parameters.
+	//
+	// [0] https://stripe.com/docs/api/payment_intents/create
+	try {
+		const paymentIntent = await stripe.paymentIntents.create(params);
+
+		// Send publishable key and PaymentIntent details to client
+		res.send({
+			clientSecret: paymentIntent.client_secret,
+		});
+	} catch (e) {
+		return res.status(400).send({
+			error: {
+				message: e.message,
+			},
+		});
 	}
 }
