785dadbb921ba265fd785b2e73ab4265a03c6816
fabbbbz <fabienlegrave@gmail.com>
2021-11-16 11:17:03 +0100

    clean routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,27 +18,29 @@ const {
 	deletedonts,
 	updateDiet,
 } = require('../controllers/userController')
-
-//Route for Sign-Up
+// SigIn SignUp
 router.post('/sign-up', signUp)
-//Route for Sign-In
 router.post('/sign-in', signIn)
-//Routes for User Profile
+// Users Info
+router.get('/me/:token', getUserInfo)
+// History 
+router.get('/history/:token', history)
+// Update 
+router.post('/update-useraddress/:token', updateUserAddress)
+router.put('/update-me/:token', updateUser)
+router.put('/update-diet', updateDiet)
+// Allergies 
+router.get('/allergies/:token/', getAllergies)
+router.delete('/delallergies/:token/:allergy', delAllergies)
+// Favorites 
 router.get('/favorites/:token', favorites)
 router.post('/favorites', favoritesAdd)
 router.delete('/favorites/:token/:meal_id', favoritesDel)
-router.post('/update-useraddress/:token', updateUserAddress)
-router.put('/update-me/:token', updateUser)
-router.get('/me/:token', getUserInfo)
-router.get('/history/:token', history)
-router.get('/allergies/:token/', getAllergies)
-router.delete('/delallergies/:token/:allergy', delAllergies)
-router.get('/myDonts/:token', donts)
-router.put('/update-diet', updateDiet)
-
-// gestion Blacklist
+// Blacklist
 router.put('/blacklist/:token', addToBlacklist)
-
+// Dont
 router.post('/adddonts/:token', adddonts)
 router.delete('/deletedonts/:token/:dont', deletedonts)
+router.get('/myDonts/:token', donts)
+
 module.exports = router
